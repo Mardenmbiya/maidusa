7abf43f99ff223c9ced81e5e1591d3e8019eb97b
Marden Mbiya <mardenmbiya@gmail.com>
2022-05-30 00:16:59 +0200

    nn

---
+++ Business.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Business| Maidusa</title>
+    <link rel="shortcut icon" href="images/logo-1.png" type="image/x-icon">
+    <link rel="stylesheet" href="style/main.css">
+    <link rel="stylesheet" href="style/responsive.css">
+</head>
+<body>
+<?php include('bare_navigation.html'); ?>
+
+</body>
+</html>
