dc5e0932b317e5d9253138489ba730ef8bcfdfb3
Marden Mbiya <mardenmbiya@gmail.com>
2022-06-02 00:18:47 +0200

    nn

--- Acceuil.php
+++ Acceuil.php
@@ -18,7 +18,7 @@
 
     </div>
     <div class="container_mag_acceuil2">
-    Articles suggerés et articles des magazines aux quels vous êtes abonnés.m
+    Articles suggerés et articles des magazines aux quels vous êtes abonnés.ll
     </div>
 
     <?php include('bare_navigation.html'); ?>
