dc1a0597ab042819ec8873a42326bb30945f0811
Marden Mbiya <mardenmbiya@gmail.com>
2022-06-03 23:43:25 +0200

    n

--- js/main.js
+++ js/main.js
@@ -59,3 +59,17 @@ function showmodal8(){
 function closemodal8(){
   document.getElementById("container_modd6").style.display="none";
 }
+
+
+var toggle_btn = document.getElementById('theme-btn');
+var body = document.getElementsByTagName('body')[0];
+var dark_theme_class = 'dark';
+toggle_btn.addEventListener('click', function() {
+ if (body.classList.contains(dark_theme_class)) {
+ body.classList.remove(dark_theme_class);
+}
+ else {
+ body.classList.add(dark_theme_class);
+}
+
+});
